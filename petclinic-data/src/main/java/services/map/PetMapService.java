2956f17f54cfa513ebd6b194317b6ae8c1a7ee48
hiiok <misq17@gmail.com>
2020-03-10 11:18:32 +0100

    Create map Services, Added one dependency in main pom.xml

---
+++ petclinic-data/src/main/java/services/map/PetMapService.java
@@ -0,0 +1,35 @@
+package services.map;
+
+import model.Pet;
+import services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapSerevice<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
--- petclinic-web/src/main/java/startup/PetClinicApplication.java
+++ petclinic-web/src/main/java/startup/PetClinicApplication.java
@@ -1,3 +1,5 @@
+package startup;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
